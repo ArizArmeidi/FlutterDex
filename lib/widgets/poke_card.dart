f6c571a5746e6455456e3ee4dd2697e6b6b18ece
Ariz Armeidi <arizarmeidi@gmail.com>
2021-03-20 22:06:15 +0700

    update poke_card.dart

--- lib/widgets/poke_card.dart
+++ lib/widgets/poke_card.dart
@@ -11,58 +11,64 @@ class PokeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.fromLTRB(20, 25, 20, 25),
-      margin: EdgeInsets.only(bottom: 20, top: 5, left: 5, right: 5),
-      decoration: BoxDecoration(
-        color: setCardColor(poke.type1.toString()),
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: setCardColor(poke.type1.toString()).withOpacity(0.5),
-            blurRadius: 5,
-            offset: Offset(2, 3),
-          ),
-        ],
-      ),
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '#' + poke.id.toString(),
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
-              ),
-              Text(
-                toBeginningOfSentenceCase(poke.name),
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 25,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  if (poke.type1 != null) TypeCard(poke.type1),
-                  SizedBox(width: 5),
-                  if (poke.type2 != null) TypeCard(poke.type2),
-                ],
-              )
-            ],
-          ),
-          Positioned(
-            right: -35,
-            bottom: -50,
-            child: Image.network(
-              poke.sprite,
-              scale: 0.5,
+    return GestureDetector(
+      onTap: () {
+        print(poke.name);
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+        margin: EdgeInsets.only(bottom: 20, top: 5, left: 5, right: 5),
+        decoration: BoxDecoration(
+          color: setCardColor(poke.type1.toString()),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: setCardColor(poke.type1.toString()).withOpacity(0.5),
+              blurRadius: 5,
+              offset: Offset(2, 3),
             ),
-          )
-        ],
+          ],
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '#' + poke.id.toString(),
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+                ),
+                Text(
+                  toBeginningOfSentenceCase(poke.name),
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    if (poke.type1 != null) TypeCard(poke.type1),
+                    SizedBox(width: 5),
+                    if (poke.type2 != null) TypeCard(poke.type2),
+                  ],
+                )
+              ],
+            ),
+            Positioned(
+              right: -35,
+              bottom: -50,
+              child: FadeInImage.assetNetwork(
+                placeholder: 'images/pokeLoad.gif',
+                image: poke.sprite,
+                imageScale: 0.5,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
